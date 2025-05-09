bfe0c0d16a218da5a17e6914682e7bd1e90f7c9c
Salah eddine khouadri <salahidslhddnn@gmail.com>
2025-05-09 03:04:35 +0200

    login and doctor signup

--- back/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
+++ back/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
@@ -37,7 +37,7 @@ public class ProjectSecurityProdConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("https://localhost:4200"));
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
